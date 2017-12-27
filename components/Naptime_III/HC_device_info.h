ebb73589fa1c755cbfb272f4e4312f452c164b1c
HDU-rib <1045602705@qq.com>
2017-12-27 17:37:56 +0800

    修改设备广播名称

--- components/Naptime_III/HC_device_info.h
+++ components/Naptime_III/HC_device_info.h
@@ -3,7 +3,7 @@
 
 // Device Information
 
-#define DEVICE_NAME       "Naptime_Tool"       /**< Name of device. Will be included in the advertising data. */
+#define DEVICE_NAME       "Naptime123"       /**< Name of device. Will be included in the advertising data. */
 #define MANUFACTURER_NAME "Huiche Inc"        /**< Manufacturer. Will be passed to Device Information Service. */
 #define FW_REV_STR        "0.2.0"
 #define HW_REV_STR        "1.0.0"             /**< Hardware Revision String. */
