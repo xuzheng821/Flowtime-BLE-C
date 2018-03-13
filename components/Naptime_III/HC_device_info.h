8c2a8e2e7929f27bd2d016c1116b8c4fb421f86b
HDU-rib <1045602705@qq.com>
2018-03-13 18:41:05 +0800

    1.0.0正式版本 1000片生产版本

--- components/Naptime_III/HC_device_info.h
+++ components/Naptime_III/HC_device_info.h
@@ -10,8 +10,8 @@
 
 // Device Information
 #define DEVICE_NAME       "易休 air"            /**< Name of device. Will be included in the advertising data. */
-#define MANUFACTURER_NAME "HZhuiche Inc"        /**< Manufacturer. Will be passed to Device Information Service. */
-#define FW_REV_STR        "1.0.0-Alpha-7"
+#define MANUFACTURER_NAME "HZhuiche Inc."        /**< Manufacturer. Will be passed to Device Information Service. */
+#define FW_REV_STR        "1.0.0"
 #define HW_REV_STR        "1.0.0"               /**< Hardware Revision String. */
 
 void mac_get(void);
