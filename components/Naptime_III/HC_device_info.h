21249c0041a4042d0c57091f8797327f98300b4b
HDU-rib <fuquansheng@entertech.cn>
2018-04-17 14:04:37 +0800

    解决问题,固件版本1.1.0,硬件版本1.0.1

--- components/Naptime_III/HC_device_info.h
+++ components/Naptime_III/HC_device_info.h
@@ -10,9 +10,9 @@
 
 // Device Information
 #define DEVICE_NAME       "易休 air"             /**< Name of device. Will be included in the advertising data. */
-#define MANUFACTURER_NAME "TEST"        /**< Manufacturer. Will be passed to Device Information Service. */
-#define FW_REV_STR        "1.0.0"                /**< software Revision String. */
-#define HW_REV_STR        "1.0.0"                /**< Hardware Revision String. */
+#define MANUFACTURER_NAME "HZhiuche Inc."        /**< Manufacturer. Will be passed to Device Information Service. */
+#define FW_REV_STR        "1.1.0"                /**< software Revision String. */
+#define HW_REV_STR        "1.0.1"                /**< Hardware Revision String. */
 
 void mac_get(void);
 void ble_devinfo_serv_init(void);
